bdf98153e2cd93be1c5e9de49d8e27f981112482
Levente Nyírő <nyiro.levente@gmail.com>
2020-05-30 18:57:27 +0200

    text change on button onPressed

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,38 @@ import "package:flutter/material.dart";
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool pressed = false;
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(primaryColor: Colors.purple[900]),
       home: Scaffold(
-        appBar: AppBar(
+        /*appBar: AppBar(
           title: Text("WordPair Generator")
-        ),
-        body: Center(
-          child: Text("Hello World")
+        ),*/
+        body: Container(
+          alignment: Alignment.center,
+          child: Column(
+            children: <Widget>[
+              RaisedButton(
+                onPressed: () {
+                  setState(() {
+                    pressed = !pressed;
+                  });
+                },
+                child: Text("Nyomj rá!"),
+              ),
+              pressed ? Text("Bekapcsolva") : Text("Kikapcsolva")
+            ],
+          )
         )
       )
     );
