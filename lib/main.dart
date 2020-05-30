225db7475689a34fc5709e97ebe9b4047a5f5ed6
Levente Nyírő <nyiro.levente@gmail.com>
2020-05-30 16:33:15 +0200

    hello world

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("Hello World", style: TextStyle(
-          fontSize: 30.5,
-          color: Colors.red[400]
+      theme: ThemeData(primaryColor: Colors.purple[900]),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("WordPair Generator")
+        ),
+        body: Center(
+          child: Text("Hello World")
         )
       )
     );
