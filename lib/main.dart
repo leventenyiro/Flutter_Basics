d34c6c3e5db53f32c424439abd364fdc15114176
Levente Nyírő <nyiro.levente@gmail.com>
2020-06-03 12:13:42 +0200

    list of data

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,37 @@
 import "package:flutter/material.dart";
 
 void main() => runApp(MaterialApp(
+  home: QuoteList(),
   debugShowCheckedModeBanner: false,
-  home: Card()
 ));
 
-class Card extends StatefulWidget {
+class QuoteList extends StatefulWidget {
   @override
-  _CardState createState() => _CardState();
+  _QuoteListState createState() => _QuoteListState();
 }
 
-class _CardState extends State<Card> {
-  int level = 0;
-  
+class _QuoteListState extends State<QuoteList> {
+
+  List<String> quotes = [
+    "Valami1",
+    "Valami2",
+    "Valami3",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text("ID Card"),
+        title: Text("Awesome Quotes"),
         centerTitle: true,
-        backgroundColor: Colors.black,
-        elevation: 0.0,
+        backgroundColor: Colors.redAccent,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            level++;
-          });
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.grey[800],
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage("assets/cch.png"),
-                radius: 40.0,
-              ),
-            ),
-            Divider(
-              height: 90.0,
-              color: Colors.grey[800],
-            ),
-            Text(
-              "NAME",
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 10.0,),
-            Text(
-              "Nyírő Levente",
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 30.0,),
-            Text(
-              "CURRECT LEVEL",
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 10.0,),
-            Text(
-              "$level",
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 30.0,),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[400],
-                ),
-                SizedBox(width: 10.0,),
-                Text(
-                  "nyiro.levente@gmail.com",
-                  style: TextStyle(
-                    color: Colors.grey[400],
-                    fontSize: 18.0,
-                    letterSpacing: 1.0,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+      body: Column(
+        children: quotes.map((q) => Text(q)).toList(),
+        /*children: quotes.map((q) {
+          return Text(q);
+        }).toList()*/
       ),
     );
   }
