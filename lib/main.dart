dcd47315571973f60cf313ae7e98be4d54d9b76e
Levente Nyírő <nyiro.levente@gmail.com>
2020-06-03 14:26:23 +0200

    extractiong widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import "quote.dart";
+import "quote_card.dart";
 
 void main() => runApp(MaterialApp(
   home: QuoteList(),
@@ -18,35 +19,6 @@ class _QuoteListState extends State<QuoteList> {
     Quote(author: "William Sheakespeare", text: "Valami2")
   ];
 
-  Widget quoteTemplate(quote) {
-    return Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              quote.text,
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.grey[600],
-              ),
-            ),
-            SizedBox(height: 6.0,),
-            Text(
-              quote.author,
-              style: TextStyle(
-                fontSize: 14.0,
-                color: Colors.grey[800],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,7 +29,7 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((q) => quoteTemplate(q)).toList(),
+        children: quotes.map((q) => QuoteCard(quote: q)).toList(),
         /*children: quotes.map((q) {
           return Text(q);
         }).toList()*/
