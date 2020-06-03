36182ae49bcdef54b47a54f23129d581bb92b2f9
Levente Nyírő <nyiro.levente@gmail.com>
2020-06-03 09:47:25 +0200

    basic

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,28 @@
 import "package:flutter/material.dart";
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool pressed = false;
-  
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primaryColor: Colors.purple[900]),
-      home: Scaffold(
-        /*appBar: AppBar(
-          title: Text("WordPair Generator")
-        ),*/
-        body: Container(
-          alignment: Alignment.center,
-          child: Column(
-            children: <Widget>[
-              RaisedButton(
-                onPressed: () {
-                  setState(() {
-                    pressed = !pressed;
-                  });
-                },
-                child: Text("Nyomj rá!"),
-              ),
-              pressed ? Text("Bekapcsolva") : Text("Kikapcsolva")
-            ],
-          )
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text("Valami"),
+      centerTitle: true,
+      backgroundColor: Colors.red[900],
+    ),
+    body: Center(
+      child: Text("Hello",
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[400],
+          fontFamily: "AbrilFatface"
         )
       )
-    );
-  }
-}
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {},
+      child: Text("Click"),
+      backgroundColor: Colors.red[900],
+    ),
+  )
+));
