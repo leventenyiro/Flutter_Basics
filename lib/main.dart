8f2d436cd05ffc4bc1313d44c39afba6a4809776
Levente Nyírő <nyiro.levente@gmail.com>
2020-06-03 11:47:27 +0200

    statefulwidget

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,14 @@ void main() => runApp(MaterialApp(
   home: Card()
 ));
 
-class Card extends StatelessWidget {
+class Card extends StatefulWidget {
+  @override
+  _CardState createState() => _CardState();
+}
+
+class _CardState extends State<Card> {
+  int level = 0;
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +22,15 @@ class Card extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.black,
         elevation: 0.0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            level++;
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
       ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
@@ -58,7 +74,7 @@ class Card extends StatelessWidget {
             ),
             SizedBox(height: 10.0,),
             Text(
-              "8",
+              "$level",
               style: TextStyle(
                 color: Colors.amberAccent[200],
                 letterSpacing: 2.0,
