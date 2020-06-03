8e2bc5c1f1034d36fa62d5a1315ec51c12a7d58b
Levente Nyírő <nyiro.levente@gmail.com>
2020-06-03 20:00:48 +0200

    worldtimeapi

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -7,27 +7,6 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-  void getData() async {
-    // simulate network request for a username
-    String username = await Future.delayed(Duration(seconds: 3), () {
-      return "Levi";
-    });
-
-    // simulate network request to get bio of the username
-    String bio = await Future.delayed(Duration(seconds: 2), () {
-      return "21 y/o, programmer";
-    });
-
-    print("$username - $bio");
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-    print("Hey there!");
-  }
-
   @override
   Widget build(BuildContext context) {
     print("build function ran");
