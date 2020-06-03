005c4efb6a95a83efdb58c1754c97143c8d9554d
Levente Nyírő <nyiro.levente@gmail.com>
2020-06-03 09:54:10 +0200

    StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,34 @@ import "package:flutter/material.dart";
 
 void main() => runApp(MaterialApp(
   debugShowCheckedModeBanner: false,
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text("Valami"),
-      centerTitle: true,
-      backgroundColor: Colors.red[900],
-    ),
-    body: Center(
-      child: Text("Hello",
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey[400],
-          fontFamily: "AbrilFatface"
-        )
-      )
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {},
-      child: Text("Click"),
-      backgroundColor: Colors.red[900],
-    ),
-  )
+  home: Home()
 ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Valami"),
+        centerTitle: true,
+        backgroundColor: Colors.red[900],
+      ),
+      body: Center(
+        child: Text("Hello",
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[400],
+            fontFamily: "AbrilFatface"
+          )
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text("Click"),
+        backgroundColor: Colors.red[900],
+      )
+    );
+  }
+}
