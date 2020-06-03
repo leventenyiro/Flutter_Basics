5f2309f93893c468749fa9ce4449ab1d6a6f80a5
Levente Nyírő <nyiro.levente@gmail.com>
2020-06-03 12:37:00 +0200

    cards

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,35 @@ class _QuoteListState extends State<QuoteList> {
     Quote(author: "William Sheakespeare", text: "Valami2")
   ];
 
+  Widget quoteTemplate(quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey[600],
+              ),
+            ),
+            SizedBox(height: 6.0,),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.grey[800],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +57,7 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((q) => Text("${q.text} - ${q.author}")).toList(),
+        children: quotes.map((q) => quoteTemplate(q)).toList(),
         /*children: quotes.map((q) {
           return Text(q);
         }).toList()*/
